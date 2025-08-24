bf9f0f559ea31204a682c5a1f38237c6966b03ca
sai <saivenkat67687@gmail.com>
2025-08-24 15:04:37 +0530

    cicd-tools-main

--- variables.tf
+++ variables.tf
@@ -8,12 +8,12 @@ variable "environment" {
 
 variable "zone_name" {
   type        = string
-  default     = "daws84s.site"
+  default     = "calvio.store"
   description = "description"
 }
 
 variable "zone_id" {
   type        = string
-  default     = "Z032558618100M4EJX8X4"
+  default     = "Z04121283NNLWE4N00ISG"
   description = "description"
 }
